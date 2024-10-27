40cb696c1cb685af2b6de2e34a126263d68139c6
ws-3917 <ws3917@outlook.com>
2024-10-27 17:17:16 +0800

    54

--- code/main.cpp
+++ code/main.cpp
@@ -7,7 +7,7 @@
 #include <string>
 #include <iostream>
 using namespace std;
-#define SCREEN_WIDTH 1280
+#define SCREEN_WIDTH 1600
 #define SCREEN_HEIGHT 960
 #define GLOBAL_FPS 60
 
@@ -47,63 +47,68 @@ int main(int argc, char *argv[])
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xff);
     SDL_RenderClear(renderer);
-    Mix_OpenAudio(0, NULL);
-
-    Mix_Music *music = Mix_LoadMUS("audio/music/arms.mp3");
-    Mix_PlayMusic(music, 0);
-
-    Mix_Chunk *chunk = Mix_LoadWAV("audio/sound/sans.mp3");
-    int fontsize = 120;
-    TTF_Font *font = TTF_OpenFont("lang/zh_CN/font/fzxs12-Pixel.ttf", fontsize);
+    int factor = 1, fontsize = 16;
+    TTF_Font *font = TTF_OpenFont("lang/zh_CN/font/unifont.otf", fontsize);
     if (!font)
         return 1;
-    const int scaler = 8;
+
     SDL_Texture *textglyph = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB32, SDL_TEXTUREACCESS_TARGET, SCREEN_WIDTH, SCREEN_HEIGHT);
-    SDL_SetTextureScaleMode(textglyph, SDL_SCALEMODE_NEAREST);
     SDL_SetRenderTarget(renderer, textglyph);
+    int start = 0x5e00, end = 0x5e00 + 5000;
+    cout << end - start << endl;
     int t1 = SDL_GetTicks();
-    SDL_FRect rect;
-    for (Uint32 ch = 0x30; ch <= 0x60; ch++)
+    SDL_FRect *rect = (SDL_FRect *)malloc(sizeof(SDL_FRect) * (end - start + 1));
+    int x = 0, y = 0;
+    for (Uint32 ch = start; ch <= end; ch++)
     {
         SDL_Surface *surface = TTF_RenderGlyph_Solid(font, ch, {0xff, 0xff, 0xff, 0xff});
         SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-        SDL_SetTextureScaleMode(texture, SDL_SCALEMODE_NEAREST);
-        rect.x = fontsize * ((ch - 0x30) % (SCREEN_WIDTH / fontsize));
-        rect.y = fontsize * ((ch - 0x30) / (SCREEN_WIDTH / fontsize));
-        rect.w = texture->w * 1.0f;
-        rect.h = texture->h * 1.0f;
-        SDL_RenderTexture(renderer, texture, nullptr, &rect);
+        bool wrap = x + texture->w > SCREEN_WIDTH;
+        if (wrap)
+        {
+            x = 0;
+            y += texture->h;
+        }
+        rect[ch - start] = {1.0f * x, 1.0f * y, texture->w * 1.0f, texture->h * 1.0f};
+        SDL_RenderTexture(renderer, texture, nullptr, &rect[ch - start]);
+        x += rect[ch - start].w * factor;
         SDL_DestroySurface(surface);
         SDL_DestroyTexture(texture);
     }
-
-    SDL_SetRenderTarget(renderer, nullptr);
     cout << SDL_GetTicks() - t1 << endl;
-    rect.w = 60;
-    rect.h = 60;
-    t1 = SDL_GetTicks();
+    SDL_Texture *buffer = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB32, SDL_TEXTUREACCESS_TARGET, SCREEN_WIDTH, SCREEN_HEIGHT);
+    SDL_SetRenderTarget(renderer, buffer);
     SDL_RenderClear(renderer);
-    for (Uint32 ch = 0x30; ch <= 0x60; ch++)
+
+    SDL_SetTextureScaleMode(textglyph, SDL_SCALEMODE_NEAREST);
+    t1 = SDL_GetTicks();
+    SDL_FRect factored_rect = {0, 0, 0, 0};
+    x = 0, y = 0;
+    for (Uint32 ch = start; ch <= end; ch++)
     {
-        rect.x = fontsize * ((ch - 0x30) % (SCREEN_WIDTH / fontsize));
-        rect.y = fontsize * ((ch - 0x30) / (SCREEN_WIDTH / fontsize));
-        SDL_RenderTexture(renderer, textglyph, &rect, &rect);
+        SDL_SetRenderTarget(renderer, buffer);
+        bool wrap = (x + rect->w * factor > SCREEN_WIDTH);
+        if (wrap)
+        {
+            x = 0;
+            y += rect[ch - start].h * factor;
+        }
+        factored_rect = {1.0f * x, 1.0f * y, rect[ch - start].w * factor * 1.0f, rect[ch - start].h * factor * 1.0f};
+        SDL_RenderTexture(renderer, textglyph, &rect[ch - start], &factored_rect);
+        x += rect[ch - start].w * factor;
+        SDL_SetRenderTarget(renderer, nullptr);
+        SDL_RenderClear(renderer);
+        SDL_RenderTexture(renderer, buffer, nullptr, nullptr);
         SDL_RenderPresent(renderer);
+        SDL_Delay(1);
     }
     cout << SDL_GetTicks() - t1 << endl;
-    for (int i = 0; i < 5; i++)
-    {
-        Mix_PlayChannel(0, chunk, 0);
-        SDL_Delay(100);
-    }
-    SDL_Delay(1000);
-    Mix_CloseAudio();
+    free(rect);
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     TTF_Quit();
     SDL_Quit();
-    Mix_Quit();
 
     return 0;
 }
